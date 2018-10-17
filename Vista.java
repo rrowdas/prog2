f92edb02b1fd17bebe55dd78f706eab39cb08243
gabriel-colman <44003544+gabriel-colman@users.noreply.github.com>
2018-10-17 10:39:27 -0300

    Update Vista.java

--- Vista.java
+++ Vista.java
@@ -1,4 +1,8 @@
 public class Vista extends Vendas{
+    public Vista(String dataVenda, String nomeVendedor, String formaPagamento) {
+        super(dataVenda, nomeVendedor, formaPagamento);
+    }
 
+    
     
 }
