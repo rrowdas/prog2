5b8b299487802e944498ca0c054e65f01137f379
Roger Daniel <rdr.roger@gmail.com>
2018-10-23 21:54:47 -0400

    main final e package swing

---
+++ Netbeans/src/MainFinal/MainFinal.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MainFinal;
+
+import PadTremBao.PadTremBao;
+import Swing.TelaInicialMDI;
+
+/**
+ *
+ * @author roger
+ */
+public class MainFinal {
+    public static void main(String[] args) {
+        PadTremBao padaria = new PadTremBao();
+        TelaInicialMDI tela = new TelaInicialMDI(padaria);
+        tela.main(null);
+                
+    }
+}
